7680e858f6a4738064d179a3f97846cf7cc85c48
YKShtykov <yakov.shtykov@gmail.com>
2016-07-07 17:13:38 +0300

    Add binary performance of double numbers and its tests

---
+++ DoublePerformance/DoubleExtencion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace DoublePerformance
+{
+  /// <summary>
+  /// Extencion class for double
+  /// </summary>
+  public static class DoubleExtencion
+  {
+    /// <summary>
+    /// Extencion method returns string of binary code of value
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static string StringPerformance(this double value)
+    {
+      StringBuilder resultString = new StringBuilder();
+      byte[] bytesArr = BitConverter.GetBytes(value);
+      BitArray bitsArr = new BitArray(bytesArr);
+
+      for (int i = 0; i < 64; i++)
+      {
+        if (bitsArr[i])
+        {
+          resultString.Insert(0, 1); 
+        }
+        else
+        {
+          resultString.Insert(0, 0);
+        }
+      }
+
+      return resultString.ToString();
+    }
+  }
+}
---
+++ DoublePerformanceTests/DoublePerformanceTests.cs
@@ -0,0 +1,88 @@
+﻿using DoublePerformance;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DoublePerformanceTests
+{
+  /// <summary>
+  /// Tests Extencion class DoublePerformance
+  /// </summary>
+  [TestClass]
+  public class DoublePerformanceTests
+  {
+    /// <summary>
+    /// Input minus value
+    /// </summary>
+    [TestMethod]
+    public void StringPerformanceTest_Minus100()
+    {
+      //Arrange
+      double testedDouble = -100;
+      string correctResult = "1100000001011001000000000000000000000000000000000000000000000000";
+      ;
+      string result;
+
+      //Act
+      result = testedDouble.StringPerformance();
+
+      //Assert
+      Assert.AreEqual(correctResult, result);
+    }
+
+    /// <summary>
+    /// Input large value
+    /// </summary>
+    [TestMethod]
+    public void StringPerformanceTest_1234567890()
+    {
+      //Arrange
+      double testedDouble = 1234567890;
+      string correctResult = "0100000111010010011001011000000010110100100000000000000000000000";
+      ;
+      string result;
+
+      //Act
+      result = testedDouble.StringPerformance();
+
+      //Assert
+      Assert.AreEqual(correctResult, result);
+    }
+
+    /// <summary>
+    /// Input maximum value of double
+    /// </summary>
+    [TestMethod]
+    public void StringPerformanceTest_MaxValue()
+    {
+      //Arrange
+      double testedDouble = double.MaxValue;
+      string correctResult = "0111111111101111111111111111111111111111111111111111111111111111";
+      ;
+      string result;
+
+      //Act
+      result = testedDouble.StringPerformance();
+
+      //Assert
+      Assert.AreEqual(correctResult, result);
+    }
+
+    /// <summary>
+    /// Input NaN
+    /// </summary>
+    [TestMethod]
+    public void StringPerformanceTest_NaN()
+    {
+      //Arrange
+      double testedDouble = double.NaN;
+      string correctResult = "1111111111111000000000000000000000000000000000000000000000000000";
+      ;
+      string result;
+
+      //Act
+      result = testedDouble.StringPerformance();
+
+      //Assert
+      Assert.AreEqual(correctResult, result);
+    }
+  }
+}
