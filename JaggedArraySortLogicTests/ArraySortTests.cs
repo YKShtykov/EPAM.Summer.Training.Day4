f736ce583258dc7f8acb24f63fe7f05c3bc9fb72
YKShtykov <yakov.shtykov@gmail.com>
2016-07-12 22:24:31 +0300

    Added absolute value sort

--- JaggedArraySortLogicTests/ArraySortTests.cs
+++ JaggedArraySortLogicTests/ArraySortTests.cs
@@ -3,6 +3,7 @@ using JaggedArraySortLogic;
 using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace JaggedArraySortLogicTests
 {
@@ -104,6 +105,29 @@ namespace JaggedArraySortLogicTests
       }
     }
 
+    [TestMethod]
+    public void BubleSortAbsoluteValueTest()
+    {
+      //Arrange
+      AbsoluteValueSorting sortingObject = new AbsoluteValueSorting();
+      int[][] resultSumArr = new int[3][];
+      resultSumArr[0] = new int[] { 2, 5, 10 };
+      resultSumArr[1] = new int[] { -100, 100 };
+      resultSumArr[2] = new int[] { 1000 };
+
+      //Act
+      ArraySort.BubleSort(testedArr, sortingObject);
+
+      //Assert
+      Debug.WriteLine("{0}", testedArr[0][0]);
+      Debug.WriteLine("{0}", testedArr[1][0]);
+      Debug.WriteLine("{0}", testedArr[2][0]);
+      for (int i = 0; i < testedArr.Length; i++)
+      {
+        CollectionAssert.AreEqual(testedArr[i], resultSumArr[i]);
+      }
+    }
+
     public class MaxValueSorting : IComparer<int[]>
     {
       public int Compare(int[] x, int[] y)
@@ -149,6 +173,16 @@ namespace JaggedArraySortLogicTests
       public int Compare(int[] x, int[] y)
       {
         return -x.Sum().CompareTo(y.Sum());
+      }
+    }
+
+    public class AbsoluteValueSorting : IComparer<int[]>
+    {
+      public int Compare(int[] x, int[] y)
+      {
+        int lhs = (Math.Abs(x.Max()) >= Math.Abs(x.Min())) ? Math.Abs(x.Max()) : Math.Abs(x.Min());
+        int rhs = (Math.Abs(y.Max()) >= Math.Abs(y.Min())) ? Math.Abs(y.Max()) : Math.Abs(y.Min());
+        return lhs.CompareTo(rhs);
       }
     }
   }
